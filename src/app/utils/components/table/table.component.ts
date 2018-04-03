9c5894d26240740400fc9d617050da6195fddc93
evroGit <evro78@gmx.de>
2018-04-03 23:15:11 +0200

    - table can get/show the data from outside, calculate the page count and show the paging, - click on delete/edit button emits/calls corresponding functions

--- src/app/utils/components/table/table.component.ts
+++ src/app/utils/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, TemplateRef} from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,22 +6,48 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
-  @Input()  tableStyle?:string = '';
-  @Input()  clientData?:any [] = [];
-
-
+  // @Input() template: TemplateRef<any>;
+  @Input() tableStyle?: string;
+  @Input() tableData: any [] = [];
+  @Input() pageSize: number = 5;
+  @Input() totalCount: number = 0;
+  currentTableData: any [] = [];
+  currentPage: number = 1;
+  pageCount: number = 1;
+  startIndex: number = 0;
+  endIndex: number = 0;
+  pages: number [] = [];
 
   constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    if (changes.tableData.currentValue) {
+      this.totalCount = this.tableData.length;
+      this.pageCount = Math.ceil(this.totalCount / this.pageSize);
+      this.pages = [...Array(this.pageCount)];
+      this.calculatePageArray();
+    }
   }
 
+  calculatePageArray() {
+    this.startIndex = (this.currentPage - 1) * this.pageSize;
+    this.endIndex = Math.min((this.startIndex + this.pageSize - 1), this.totalCount - 1);
+    this.currentTableData = this.tableData.slice(this.startIndex, this.endIndex);
+  }
+
+  onPagingClick(page) {
+    this.currentPage = page;
+    this.calculatePageArray();
+  }
+
+  @Output() deleteItem = new EventEmitter<any>();
   onDeleteClick(item: any) {
-
+    this.deleteItem.emit(item);
   }
 
+  @Output() editItem = new EventEmitter<any>();
   onEditClick(item: any) {
-
+    this.editItem.emit(item);
   }
 }
